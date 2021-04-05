7d4692393d30a5a1066ae4daca3e8919626cdcb7
jan.szczekulski <jan.szczekulski@thehut.local>
2021-04-05 12:31:11 +0100

    Add solution in google code jam suitable format

--- 2020/round1A/pascal_walk/pascal_walk_input_ver.py
+++ 2020/round1A/pascal_walk/pascal_walk_input_ver.py
@@ -14,9 +14,8 @@ def how_many_1s_to_fill(nr):
     return nr - row_nr - output_sum_of_row(row_nr)
 
 
-
-
-
+def output_sum_of_row(n):
+    return 2**(n-1)
 
 
 def print_road(nr):
@@ -29,48 +28,27 @@ def print_road(nr):
     row_nr = find_actual_row(nr)
 
     # Waste around
-    print("Wasting around:")
     for _ in range(int(waste_moves/2)):
         print("1 1")
         print("1 2")
-    print()
 
     # Travel to approp row
-    print("Travel to approp row:")
     for i in range(row_nr):
         print(f"1 {i+1}")
-    print()
 
     # Travel through that row
-    print("Traverse through approp row:")
     for i in range(row_nr):
         print(f"{i+1} {row_nr}")
-    print()
 
 
     # Waste one final move if needed
-    print("Waste one final move if needed:")
     if waste_moves%2 == 1:
         print(f"{row_nr} {row_nr+1}") # Fill this in 
     
 
 
-def competition():
-    #cases=int(input())
-    file_name = "test1.txt"
-    file_handle = open(file_name)  
-    cases = int(next(file_handle))
-
-    for case in range(1,cases+1):
-        nr = int(next(file_handle))
-        print(f"Case #{case}:")
-        print_road(nr)
-    file_handle.close()
-
-
-def output_sum_of_row(n):
-    return 2**(n-1)
-
-
-if __name__=='__main__':
-    competition()
+cases=int(input())
+for case in range(1,cases+1):
+    nr = int(input())
+    print(f"Case #{case}:")
+    print_road(nr)
