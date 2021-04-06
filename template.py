3de5bb212c560b872da81e0de855a851d8176ddd
jan.szczekulski <jan.szczekulski@thehut.local>
2021-04-06 23:04:20 +0100

    I swear its the last template update

--- template.py
+++ template.py
@@ -1,13 +1,19 @@
 def main():
-    return
+    pass
 
 #cases=int(input())
-file_name = "test1.txt"
-file_handle = open(file_name)  
-cases = int(next(file_handle))
+def competition():
+    file_name = "test1.txt"
+    file_handle = open(file_name)
+    cases = int(next(file_handle))
 
-for case in range(1,cases+1):
-    nr1, nr2, letters = map(int,next(file_handle).split())
-    answer = main()
-    print(f"Case #{case}: {answer}")
-file_handle.close()
+    for case in range(1,cases+1):
+        nr1, nr2 = map(int,next(file_handle).split())
+        answer = main()
+        print(f"Case #{case}: {answer}")
+    file_handle.close()
+
+
+
+if __name__=="__main__":
+    competition()
